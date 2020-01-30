5d8eccb85128bd29df0d0cbc42decde0e4c30d83
William Silversmith <william.silversmith@gmail.com>
2020-01-29 19:19:01 -0500

    fix: export fpzip.js functions

--- fpzip.js
+++ fpzip.js
@@ -3,19 +3,19 @@ let lib = nbind.init().lib;
 
 function _decompress(data, fn) {
   let zip = lib.Fpzip(data.buffer);
-  let out_buffer = (zip.get_type() === 0) 
+  let out = (zip.get_type() === 0) 
     ? (new Float32Array(zip.nvoxels()))
     : (new Float64Array(zip.nvoxels()));
-  zip[fn](data, out_buffer);
-  return out_buffer;
+  zip.decompress(data.buffer, out.buffer);
+  return out;
 }
  
-function decompress(data) {
+exports.decompress = function (data) {
   return _decompress(data, 'decompress');
-}
+};
 
-function dekempress(data) {
+exports.dekempress = function (data) {
   return _decompress(data, 'dekempress');
-}
+};
 
 
